b22b1d497ca678221f8bdf7341dbb0b7e78193df
Tarun julmi <julmetech@gmail.com>
2026-06-26 23:02:26 +0530

    removed unwanted resource

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,3 @@
-output "oidc_provider_arn" {
-  description = "The ARN of the GitHub Actions OIDC provider"
-  value       = aws_iam_openid_connect_provider.github.arn
-}
-
 output "vpc_id" {
   description = "VPC id"
   value       = aws_vpc.this.id
